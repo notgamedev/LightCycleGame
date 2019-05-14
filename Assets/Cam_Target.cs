5e0aa6e11d1005f1622690ea41bcc7c55b8d330c
notgamedev <paulo.minvielle@gmail.com>
2019-05-14 12:51:45 +0200

    added cam target

--- Assets/Cam_Target.cs
+++ Assets/Cam_Target.cs
@@ -2,15 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cam_Target : MonoBehaviour {
+ 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class CamTarget : MonoBehaviour
+
+{
+
+    public Transform target;
+    public float distance;
+    public float height;
+    public float rotationDamping;
+    public float heightDamping;
+
+    float targetRotationAngle;
+    float targetHeight;
+    float currentRotationAngle;
+    float currentHeight;
+
+    private Quaternion currentRotation;
+
+
+    void LateUpdate()
+
+    {
+
+        if (target != null)
+
+        {
+
+            //Calcul de la rotation et hauteur cible.
+
+            targetRotationAngle = target.eulerAngles.y;
+            targetHeight = target.position.y + height;
+
+
+
+            currentRotationAngle = transform.eulerAngles.y;
+            currentHeight = transform.position.y;
+
+
+
+            currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, targetRotationAngle, rotationDamping * Time.deltaTime);
+
+            currentHeight = Mathf.Lerp(currentHeight, targetHeight, heightDamping * Time.deltaTime);
+
+            currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+
+
+
+            transform.position = target.position;
+            transform.position -= currentRotation * Vector3.forward * distance;
+
+
+
+            transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+            transform.LookAt(target);
+
+
+
+        }
+
+    }
+
 }
